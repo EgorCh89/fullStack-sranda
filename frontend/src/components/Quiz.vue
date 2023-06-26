9bdf9de859e82e9db8956a507d4170104c5ba431
Egor <egor.chekunov@student.gyarab.cz>
2023-06-26 16:29:21 +0200

    quiz !

--- frontend/src/components/Quiz.vue
+++ frontend/src/components/Quiz.vue
@@ -7,32 +7,27 @@ const props = defineProps({
   },
 });
 const isSubmit = ref(false);
-const isTrue = ref([]);
-isTrue.value.push(ref([]));
-isTrue.value.push(ref([]));
-isTrue.value[1].value.push(ref(false));
-console.log(isTrue.value);
 const checkboxinput = ref([]);
-console.log(checkboxinput.value);
-for (let i = 0; i < props.questions.length; i++) {
-  let row = ref([]);
-  for (let j = 0; j < props.questions[i].answers.length; j++) {
-    row.value.push(ref(false));
-  }
+const showRight = ref(false);
+const score = ref(0);
 
-  checkboxinput.value.push(row.value);
-  console.log(checkboxinput.value);
+for (let i = 0; i < props.questions.length; i++) {
+  checkboxinput.value.push(ref());
 }
-console.log("------------------");
-console.log(checkboxinput.value);
 /*
           v-model="checkboxinput.value[i][j]"
 
 */
-
 function Check() {
   isSubmit.value = true;
-  let score = 0;
+  for (let i = 0; i < props.questions.length; i++) {
+    if (
+      checkboxinput.value[i] ===
+      props.questions[i].answers[props.questions[i].right]
+    ) {
+      score.value++;
+    }
+  }
 }
 </script>
 
@@ -45,20 +40,81 @@ function Check() {
           required
           class="form-check-input"
           type="radio"
+          :value="ans"
           :name="question.question"
           :id="ans + question.answers + question"
-          v-model="checkboxinput[i][j]"
+          v-model="checkboxinput[i]"
         />
         <label
           class="form-check-label"
           :for="ans + question.answers + question"
         >
-          {{ ans }} [{{ i }}|{{ j }}]
+          {{ ans }}
         </label>
       </div>
     </div>
     <button type="submit" class="btn btn-primary">Send</button>
   </form>
-  <div v-if="isSubmit">Your score: 69 {{ isTrue }}</div>
-  value = {{ checkboxinput }}
+  <div v-if="isSubmit">
+    Your score: {{ score }}/{{ questions.length }}
+
+    <button v-if="!showRight" @click="showRight = true" class="btn btn-primary">
+      show quiz
+    </button>
+  </div>
+
+  <div v-if="showRight">
+    <div v-for="(question, i) in questions">
+      <h2>{{ question.question }}</h2>
+      <div class="form-check" v-for="(ans, j) in question.answers">
+        <input
+          :checked="checkboxinput[i] === ans ? true : false"
+          class="form-check-input"
+          type="radio"
+          :name="question.question"
+          :id="ans + question.answers + question"
+          :class="
+            j === question.right
+              ? 'right'
+              : checkboxinput[i] === ans
+              ? 'wrong'
+              : ''
+          "
+        />
+        <label
+          class="form-check-label"
+          :class="
+            j === question.right
+              ? 'right'
+              : checkboxinput[i] === ans
+              ? 'wrong'
+              : ' '
+          "
+          :for="ans + question.answers + question"
+        >
+          {{ ans }}
+        </label>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+.right {
+  background-color: green;
+}
+.wrong {
+  background-color: red;
+}
+.right:disabled {
+  background-color: green;
+}
+.wrong:disabled {
+  background-color: red;
+}
+
+.form-check-input:disabled {
+  background-color: inherit;
+  opacity: 1;
+}
+</style>
--- frontend/src/views/QuizPage.vue
+++ frontend/src/views/QuizPage.vue
@@ -1,21 +1,27 @@
 <script setup>
+import Calc from "../components/Calc.vue";
 import Quiz from "../components/Quiz.vue";
 
 let questions = [
   {
-    question: "How do you do?",
-    answers: ["good!", "bad", "cool", "Idk"],
-    right: 0,
+    question: "2 + 2 is?",
+    answers: ["2", "4", "8", "-1/12"],
+    right: 1,
   },
   {
-    question: "Do you want to sleep?",
-    answers: ["Yes!", "No", "WHAT?", "Idk"],
-    right: 1,
+    question: "how many days in February?",
+    answers: ["27", "28", "29", "it depends"],
+    right: 3,
   },
   {
     question: "Do you like Masa?",
-    answers: ["Yes!", "Yessss", "Hell Yea!", "YES!!!"],
+    answers: ["No", "Yes!", "No", "mb"],
     right: 1,
+  },
+  {
+    question: "Do you play House Fliper?",
+    answers: ["Yes!", "No", "Long time ago"],
+    right: 0,
   },
 ];
 </script>
